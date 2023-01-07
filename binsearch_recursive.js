af7914f15a76350c5fdb5ed58ab14e34a2d44655
ahrayahlabcoimin <ahrayahlabcoimin@gmail.com>
2023-01-06 21:05:28 -0600

    Add basic tests for recurisvely binary search

---
+++ binsearch_recursive.js
@@ -0,0 +1,10 @@
+import { strict as assert} from 'node:assert';
+
+function binarySearch(arr, target) {
+
+}
+  
+# https://www.techiedelight.com/binary-search/
+assert.equal(binarySearch([2,3,5,7,9], 7), 3)
+assert.equal(binarySearch([1,4,5,8,9], 2), -1)
+
