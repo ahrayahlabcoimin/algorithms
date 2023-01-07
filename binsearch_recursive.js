e53c27a877f13e63c52fdfe748d052e1644fac97
ahrayahlabcoimin <ahrayahlabcoimin@gmail.com>
2023-01-06 21:31:01 -0600

    Fix syntax error

--- binsearch_recursive.js
+++ binsearch_recursive.js
@@ -4,7 +4,7 @@ function binarySearch(arr, target) {
 
 }
   
-# https://www.techiedelight.com/binary-search/
+// https://www.techiedelight.com/binary-search/
 assert.equal(binarySearch([2,3,5,7,9], 7), 3)
 assert.equal(binarySearch([1,4,5,8,9], 2), -1)
 
