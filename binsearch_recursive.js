8f01e8f788efcb10c4d8da165f8274042baed12d
ahrayahlabcoimin <ahrayahlabcoimin@gmail.com>
2023-01-06 21:31:33 -0600

    Implement recursive binary search

--- binsearch_recursive.js
+++ binsearch_recursive.js
@@ -1,7 +1,16 @@
 import { strict as assert} from 'node:assert';
 
-function binarySearch(arr, target) {
-
+function binarySearch(arr, target, start=0, end=arr.length-1) {
+	let mid = Math.floor((end + start)/2);
+	if (target == arr[mid]) {
+		return mid;
+	} else if (end <= start) {
+		return -1;
+	} else if (target < arr[mid]) {
+		return binarySearch(arr, target, 0, mid-1)
+	} else {
+		return binarySearch(arr, target, mid+1, end)
+	}
 }
   
 // https://www.techiedelight.com/binary-search/
